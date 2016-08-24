3a905f898c81d6465aed6f196d6b1a8152a912ba
Frederick Cheung <frederick.cheung@gmail.com>
2016-08-24 18:36:22 +0100

    setup rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bolt'
+Dir[File.dirname(__FILE__)+'/support/*.rb'].each {|f| require f }
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = "./spec/examples.txt"
+  config.mock_with :rspec do |c|
+    c.syntax = [:expect]
+    c.verify_partial_doubles = true
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:expect]
+  end
+end
---
+++ spec/support/match_hex.rb
@@ -0,0 +1,6 @@
+RSpec::Matchers.define :match_hex do |y|
+  match do |x|
+    formatted = x.unpack('H*')[0].chars.each_slice(2).flat_map {|pair| pair.join}.join(':')
+    expect(formatted).to eq(y)
+  end
+end
