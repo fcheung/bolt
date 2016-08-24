c5742e51ce4d41afed20ceed5407af1cc16cbe5f
Frederick Cheung <frederick.cheung@gmail.com>
2016-08-24 19:03:47 +0100

    byebug is handy in dev

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in bolt.gemspec
 gemspec
+gem 'byebug'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'bolt'
 Dir[File.dirname(__FILE__)+'/support/*.rb'].each {|f| require f }
-
+require 'byebug'
 RSpec.configure do |config|
   config.example_status_persistence_file_path = "./spec/examples.txt"
   config.mock_with :rspec do |c|
