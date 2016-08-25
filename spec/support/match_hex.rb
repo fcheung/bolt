f585cb97c9d1d2228c7aa1c05826ad34cb50ecad
Frederick Cheung <frederick.cheung@gmail.com>
2016-08-25 09:56:49 +0100

    make match_hex matcher produce nicer output

--- spec/support/match_hex.rb
+++ spec/support/match_hex.rb
@@ -1,6 +1,13 @@
-RSpec::Matchers.define :match_hex do |y|
-  match do |x|
-    formatted = x.unpack('H*')[0].chars.each_slice(2).flat_map {|pair| pair.join.upcase}.join(':')
-    expect(formatted).to eq(y)
+RSpec::Matchers.define :match_hex do |expected|
+  match do |actual|
+    actual == [expected.gsub(':', '')].pack('H*')
+  end
+
+  failure_message do |actual|
+    "expected that #{format_bytes(actual)} would be equal to #{expected}"
+  end
+
+  def format_bytes bytes
+    bytes.unpack('H*')[0].chars.each_slice(2).flat_map {|pair| pair.join.upcase}.join(':')
   end
 end
